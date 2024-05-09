d609ecfdbe6df3165541a75b2449f3e7365e8bcf
Hi0x <hi@hi0x.net>
2024-05-09 05:56:13 +0300

    Update search.js

--- src/pages/domain/search.js
+++ src/pages/domain/search.js
@@ -16,7 +16,7 @@ export default function SearchPage() {
       <Flex
         align="center"
         justify="center"
-        bg={useColorModeValue("white", "gray.700")}
+        bg={"#fff0"}
         borderRadius="md"
         color={useColorModeValue("gray.700", "whiteAlpha.900")}
         shadow="base"
